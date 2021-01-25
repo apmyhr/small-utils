c6ce02fbd36ac6b0ac8b5f65cc5d439da851c9f7
apmyhr <aaron.myhr@live.com>
2021-01-25 16:52:14 -0500

    add icon pack locally

--- src/main.js
+++ src/main.js
@@ -3,6 +3,10 @@ import App from './App.vue'
 import vuetify from './plugins/vuetify';
 import store from './store'
 
+//This makes the material design icons available offline, but it adds about 3MB to the deployment.
+//To use this, run yarn add @mdi/font -D
+//import '@mdi/font/css/materialdesignicons.css'
+
 Vue.config.productionTip = false
 
 new Vue({
