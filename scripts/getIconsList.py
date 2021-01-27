f8ca53386b834ce274e9ab7c12fbd9500a58645c
apmyhr <aaron.myhr@live.com>
2021-01-27 16:19:03 -0500

    add fontawesome icons

--- scripts/getIconsList.py
+++ scripts/getIconsList.py
@@ -2,26 +2,57 @@
 
 import os
 import json
+import yaml
 
 script_dir = os.path.dirname(__file__)
 print ("Running in {}".format(script_dir))
 
-filename = os.path.join(script_dir, '../node_modules/@mdi/font/css/materialdesignicons.css')
+# Mdi filenames.  Array in case we add more of them.
+filenames = [
+    os.path.join(script_dir, '../node_modules/@mdi/font/css/materialdesignicons.css'),
+]
+
+font_awesome_filename = os.path.join(script_dir, '../node_modules/@fortawesome/fontawesome-free/metadata/icons.yml')
+
 output_path = os.path.join(script_dir, "../public/data/iconList.json")
 
 icons = []
+fab_icons = []
 
-# Read from the CSS file
-print("Attempting to read {}".format(filename))
-with open(filename) as f:
-    content = f.readlines()
 
-for line in content:
-    # Lines with the icon name will have a ::before
-    # .mdi-abacus::before {
-    parts = line.split("::before")
-    if len(parts) == 2:
-        icons.append(parts[0][1:])
+for filename in filenames:
+    # Read from the CSS file
+    print("Attempting to read {}".format(filename))
+    with open(filename) as f:
+        content = f.readlines()
+
+    for line in content:
+        # Lines with the icon name will have a :before
+        # .mdi-abacus::before {
+        parts = line.split("::before")
+        if len(parts) == 2:
+            icon_name = parts[0]
+            # Trim any begining period
+            icon_name = icon_name.lstrip('.')
+
+            # Ignore these lines, they are not valid
+            if icon_name.startswith("&"):
+                continue
+
+            icons.append(icon_name)
+
+# Process Font Awesome using the YAML file
+print("Attempting to read {}".format(font_awesome_filename))
+with open(font_awesome_filename, 'r') as f:
+    data = yaml.load(f, Loader=yaml.FullLoader)
+    print("Found {} Icons from icons.YML".format(len(data)))
+
+for icon_name in data:
+    icon_data = data[icon_name]
+    if 'brands' in icon_data['styles']:
+        icons.append('fab fa-' + icon_name)
+    else:
+        icons.append('fas fa-' + icon_name)
 
 # Write the list of icons to a file
 print("Writting {} icons to {}".format(len(icons), output_path))
--- src/main.js
+++ src/main.js
@@ -3,9 +3,11 @@ import App from './App.vue'
 import vuetify from './plugins/vuetify';
 import store from './store'
 
-//This makes the material design icons available offline, but it adds about 3MB to the deployment.
-//To use this, run yarn add @mdi/font -D
+//This makes the icons available offline, but it adds about 3MB to the deployment.
+//yarn add @mdi/font -D
+//yarn add @fortawesome/fontawesome-free -D
 //import '@mdi/font/css/materialdesignicons.css'
+//import '@fortawesome/fontawesome-free/css/all.css' 
 
 Vue.config.productionTip = false
 
