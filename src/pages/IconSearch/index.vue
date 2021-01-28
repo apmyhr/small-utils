a949037ae58a9e1bfd27c26c1e6dabbdf9fcee96
apmyhr <aaron.myhr@live.com>
2021-01-28 08:36:44 -0500

    add a debounce to filtering

--- src/pages/IconSearch/index.vue
+++ src/pages/IconSearch/index.vue
@@ -1,5 +1,11 @@
 <template>
-  <v-card tile elevation="16" class="mx-auto pageCard" v-resize="onResize" id="iconListCard">
+  <v-card
+    tile
+    elevation="16"
+    class="mx-auto pageCard"
+    v-resize="onResize"
+    id="iconListCard"
+  >
     <v-card-title class="white--text orange darken-4">
       Icon Search
       <v-spacer></v-spacer>
@@ -8,9 +14,14 @@
       </v-btn>
     </v-card-title>
     <v-card-title>
-      <v-text-field v-model="filterText" clearable></v-text-field>
+      <v-text-field clearable @input="updateFilterText"></v-text-field>
     </v-card-title>
-    <v-virtual-scroll :items="iconRows" height="600" :item-height="iconCardSize.height" bench="10">
+    <v-virtual-scroll
+      :items="iconRows"
+      height="600"
+      :item-height="iconCardSize.height"
+      bench="10"
+    >
       <template v-slot:default="{ item }">
         <v-card
           class="d-flex align-content-start justify-center flex-wrap"
@@ -29,11 +40,13 @@
           >
             <v-icon size="88" class="mx-8">{{ icon }}</v-icon>
             <v-card-actions class="caption pa-0 ma-0">
-            <v-card-subtitle class="caption pa-0 ma-0 ml-2 text-truncate">{{ icon }}
-            </v-card-subtitle>
-            <v-spacer></v-spacer>
+              <v-card-subtitle class="caption pa-0 ma-0 ml-2 text-truncate"
+                >{{ icon }}
+              </v-card-subtitle>
+              <v-spacer></v-spacer>
               <v-btn small icon @click="copyText(icon)"
-              ><v-icon small> mdi-content-copy </v-icon></v-btn>
+                ><v-icon small> mdi-content-copy </v-icon></v-btn
+              >
             </v-card-actions>
           </v-card>
         </v-card>
@@ -44,6 +57,7 @@
 
 <script>
 import * as arrayUtils from "../../utils/arrayUtils";
+import * as functionUtils from "../../utils/functionUtils";
 
 export default {
   name: "IconSearch",
@@ -65,8 +79,8 @@ export default {
         throw err;
       });
   },
-  mounted () {
-    this.onResize()
+  mounted() {
+    this.onResize();
   },
   data() {
     return {
@@ -80,7 +94,7 @@ export default {
       iconCardSize: {
         width: 152,
         height: 124,
-      }
+      },
     };
   },
   computed: {
@@ -111,22 +125,29 @@ export default {
      */
     iconRows() {
       //Figure out row length based on width
-      let rowLength = Math.max(Math.floor(this.iconListSize.width / this.iconCardSize.width), 1);
+      let rowLength = Math.max(
+        Math.floor(this.iconListSize.width / this.iconCardSize.width),
+        1
+      );
 
       let rows = arrayUtils.divideIntoRows(this.filteredIcons, rowLength);
 
-      console.log(rows);
       return rows;
     },
   },
   methods: {
+    updateFilterText: functionUtils.debounce(function (newValue) {
+      this.filterText = newValue;
+    }, 500),
     copyText(text) {
       navigator.clipboard.writeText(text);
     },
-    onResize () {
+    onResize() {
       let element = document.getElementById("iconListCard");
-      this.iconListSize = { width: element.clientWidth, height: element.clientHeight }
-      console.log(this.iconListSize);
+      this.iconListSize = {
+        width: element.clientWidth,
+        height: element.clientHeight,
+      };
     },
   },
 };
