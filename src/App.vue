08e1010817e81671cbe80c96314bcc642f291821
apmyhr <aaron.myhr@live.com>
2021-01-29 11:14:29 -0500

    add a home page

--- src/App.vue
+++ src/App.vue
@@ -1,65 +1,66 @@
 <template>
   <v-app>
     <v-app-bar dense app color="primary" dark>
-      <v-app-bar-nav-icon @click="drawer = true"><v-icon>mdi-menu</v-icon></v-app-bar-nav-icon>
-
-      <v-app-bar-title style="position: absolute; left: 48px">
-        SmallUtils.com
-        <v-btn icon href="https://github.com/apmyhr/small-utils" target="blank">
-          <v-icon>mdi-github</v-icon>
-        </v-btn>
-      </v-app-bar-title>
+      <v-app-bar-nav-icon @click="drawer = true"
+        ><v-icon>mdi-menu</v-icon></v-app-bar-nav-icon
+      >
 
       <v-spacer></v-spacer>
 
       <v-app-bar-title>
-        {{selectedPage.title}}
-        </v-app-bar-title>
+        {{ selectedPage.title }}
+      </v-app-bar-title>
 
       <v-spacer></v-spacer>
 
       <v-btn icon @click="darkMode = !darkMode">
         <v-icon>mdi-invert-colors</v-icon>
       </v-btn>
+      <v-btn icon href="https://github.com/apmyhr/small-utils" target="blank">
+        <v-icon>mdi-github</v-icon>
+      </v-btn>
     </v-app-bar>
 
-    <v-navigation-drawer
-      v-model="drawer"
-      absolute
-      temporary
-    >
-      <v-list
-        nav
-        dense
-      >
-        <v-list-item-group
-          v-model="selectedPageNumber"
-        >
+    <v-navigation-drawer v-model="drawer" absolute temporary>
+      <v-list nav dense>
+        <v-list-item-group v-model="selectedPageNumber">
           <v-list-item v-for="(page, index) in pages" v-bind:key="index">
             <v-list-item-icon>
-              <v-icon>{{page.icon}}</v-icon>
+              <v-icon>{{ page.icon }}</v-icon>
             </v-list-item-icon>
-            <v-list-item-title>{{page.title}}</v-list-item-title>
+            <v-list-item-title>{{ page.title }}</v-list-item-title>
           </v-list-item>
         </v-list-item-group>
       </v-list>
+
+      <template v-slot:append>
+        <v-list-item href="/">
+          <v-list-item-icon>
+            <v-icon>mdi-home-city</v-icon>
+          </v-list-item-icon>
+          <v-list-item-title>SmallUtils.com</v-list-item-title>
+        </v-list-item>
+      </template>
     </v-navigation-drawer>
 
     <v-main class="mainSection">
       <IconSearch v-if="selectedPage.title == 'Icon Search'"></IconSearch>
+      <Home v-else></Home>
     </v-main>
   </v-app>
 </template>
 
 <script>
-import pages from "./configs/pages.json"
+import pages from "./configs/pages.json";
 
-import IconSearch from "./pages/IconSearch"
+import Home from "./pages/Home";
+import IconSearch from "./pages/IconSearch";
 
 export default {
   name: "App",
 
   components: {
+    Home,
     IconSearch,
   },
 
@@ -68,16 +69,24 @@ export default {
   },
 
   data: () => ({
+    homePage: { title: "Small Utils", icon: "mdi-tools" },
     drawer: false,
     selectedPageNumber: 0,
   }),
 
   computed: {
-    pages(){
+    pages() {
       return pages.pages;
     },
-    selectedPage(){
-      return this.pages[this.selectedPageNumber]
+    selectedPage() {
+      if (
+        typeof this.selectedPageNumber != "undefined" &&
+        this.selectedPageNumber != null
+      ) {
+        return this.pages[this.selectedPageNumber];
+      } else {
+        return this.homePage;
+      }
     },
     darkMode: {
       get: function () {
@@ -93,7 +102,7 @@ export default {
 </script>
 
 <style scoped>
-.mainSection{
+.mainSection {
   height: calc(100% - 48px);
 }
 </style>
---
+++ src/pages/Home/index.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-container class="pa-4 text-center">
+    <v-row align="center" justify="center">
+      <template v-for="(page, index) in pages">
+        <v-col :key="index" cols="12" md="4">
+          <v-hover v-slot="{ hover }">
+            <v-card :elevation="hover ? 12 : 2" :class="{ 'on-hover': hover }">
+              <v-icon size="124">{{ page.icon }}</v-icon>
+              <v-card-text class="my-4 text-center title">
+                {{ page.title }}
+              </v-card-text>
+              <v-card-subtitle class="my-4 text-center">{{
+                page.description
+              }}</v-card-subtitle>
+            </v-card>
+          </v-hover>
+        </v-col>
+      </template>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import pages from "../../configs/pages.json";
+
+export default {
+  name: "Home",
+  data() {
+    return {};
+  },
+  computed: {
+    pages() {
+      return pages.pages;
+    },
+  },
+};
+</script>
+
+<style>
+</style>
