c47337ed4055519c1eab987b419c1d1ef58e4b56
apmyhr <aaron.myhr@live.com>
2021-01-31 12:38:40 -0500

    fix back button experience

--- src/App.vue
+++ src/App.vue
@@ -80,6 +80,14 @@ export default {
     if (pathName) {
       this.setPageFromUrl(pathName);
     }
+
+    let myThis = this;
+
+    window.onpopstate = function(event) {
+      pathName = window.location.pathname.substring(1);
+      console.log(`onpopstate - location: ${document.location}, state: ${JSON.stringify(event.state)}`)
+      myThis.setPageFromUrl(pathName);
+    }
   },
 
   data: () => ({
@@ -130,7 +138,9 @@ export default {
   watch: {
     selectedPage(page){
       //Store the URL in history
-      history.pushState({}, page.title, page.url);
+      if (window.location.pathname != '/' + page.url){
+        history.pushState({}, page.title, page.url);
+      }
     }
   }
 };
